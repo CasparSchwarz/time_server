a7d227515bf823957f67b43b4e17358853db2738
Caspar Schwarz <caspar.s@gmx.de>
2016-12-05 19:13:27 +0100

    everything inside server.js did not work out

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http')
 
 var server = http.createServer(function(req, res){
   res.statusCode = 200;
-  res.end("");
+  res.end("Das heutige Darum");
 });
 server.listen(10, function(){
   console.log("Server running at http://localhost;10");
--- time.js
+++ time.js
@@ -1,1 +1,7 @@
 <script type='text/javascript'>
+var Date = new Date();
+var Day = Date.getDate();
+var Month = Date.getMonth();
+var Year = Date.getFullYear();
+
+document.write("Heute ist der " + Day + "." + Month + "." + Year);
