ff31c7f570e0a4595a421c078fa4c4b5be70ecbb
Caspar Schwarz <caspar.s@gmx.de>
2016-12-05 19:48:52 +0100

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http')
-
-var server = http.createServer(function(req, res){
-  res.statusCode = 200;
-  res.end(index.html);
-});
-server.listen(10, function(){
-  console.log("Server running at http://localhost;10");
-})
