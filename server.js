e3088c5a06b54189523bea4aa04387bb734a845c
Caspar Schwarz <caspar.s@gmx.de>
2016-12-05 18:57:10 +0100

    first change of server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http')
 
 var server = http.createServer(function(req, res){
   res.statusCode = 200;
-  res.end("[Present Time]");
+  res.end("<?php include ("time.php"); ?>");
 });
 server.listen(10, function(){
   console.log("Server running at http://localhost;10");
