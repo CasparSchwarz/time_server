b58d0fc8837d19476d886552e81f24fb2dacff41
Caspar Schwarz <caspar.s@gmx.de>
2016-12-05 19:43:06 +0100

    everything works despite the month is incorrect

--- server_2.js
+++ server_2.js
@@ -1,8 +1,18 @@
 var http = require('http');
+var fs = require('fs');
+
+
 
 function onRequest(request, response){
   console.log("A user made a request" + request.url);
   response.end("Hallo Pisser");
+}
+
+function onRequest(request, response){
+  if(request.method == 'GET' && request.url == '/'){
+    response.writeHead(200, {"Content-Type": "text/html"});
+    fs.createReadStream("./index.html").pipe(response);
+  }
 }
 
 http.createServer(onRequest).listen(80);
