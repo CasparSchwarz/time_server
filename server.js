66df135128b014f8aa3633de70e72fc6e5f06727
Caspar Schwarz <caspar.s@gmx.de>
2016-12-05 19:07:22 +0100

    js file added and server.js changed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http')
 
 var server = http.createServer(function(req, res){
   res.statusCode = 200;
-  res.end("<?php include ("time.php"); ?>");
+  res.end("");
 });
 server.listen(10, function(){
   console.log("Server running at http://localhost;10");
